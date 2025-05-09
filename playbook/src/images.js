8403323c5347d4af9245b8aa75306e3597e2048a
joeopenpartners <joewoodcock1@googlemail.com>
2025-05-09 14:07:23 +0100

    Fix image loading and add error handling

---
+++ playbook/src/images.js
@@ -0,0 +1,13 @@
+// Import all images
+import handshakeGif from './assets/handshake.gif';
+import goalHero from './assets/hikers.jpg';
+import monsterGif from './assets/monster2.gif';
+import personHero from './assets/person.jpg';
+
+// Export all images
+export {
+  handshakeGif,
+  goalHero,
+  monsterGif,
+  personHero
+}; 
